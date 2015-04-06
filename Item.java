a8a2dcc3c07b041e4574ce7e2bd58c5464fc7c30
ScottJennings <whimsicalpacifist@gmail.com>
2015-04-05 22:42:57 -0400

    Placing Items
    Placing Items in rooms

---
+++ Item.java
@@ -0,0 +1,39 @@
+
+/**
+ * Item class -  an item in an adventure game.
+ * 
+ * @author Scott Jennings
+ * @version 04/06/2015
+ */
+public class Item
+{
+    // instance variables - replace the example below with your own
+    private String description;
+    private int weight;
+
+    public Item(String desc, int wgt)
+    {
+        description = desc;
+        weight = wgt;
+    }
+
+    /**
+     * Get description
+     * 
+     * @return the item description
+     */
+    public String getDescription()
+    {
+        return description; 
+    }
+    
+    /**
+     * get weight
+     * 
+     * @return the item weight
+     */
+    public int getWeight()
+    {
+        return weight;
+    }
+}
